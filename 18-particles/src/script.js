a6765d75e4f068cfbc12ecf4fe5337931c27ab37
TareefIman <tareef.iman.azizi@gmail.com>
2024-02-10 19:26:29 +0800

    stopped at minute 25

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -2,6 +2,8 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
 
+THREE.ColorManagement.enabled = false;
+
 /**
  * Base
  */
@@ -18,24 +20,37 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load("/textures/particles/2.png");
 
 /**
- * Particles
- */
+ * Particles */
 // Geometry
-const particlesGeometry = new THREE.BufferGeometry(1, 32, 32);
-const count = 500;
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 50000;
 
-const position = new Float32Array();
+const positions = new Float32Array(count * 3);
 
-// Material
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10;
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3),
+);
+//Material
 const particlesMaterial = new THREE.PointsMaterial();
 particlesMaterial.size = 0.02;
 particlesMaterial.sizeAttenuation = true;
+particlesMaterial.color = new THREE.Color("#ff88cc");
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particleTexture;
+particlesMaterial.alphaTest = 0.001;
 
-//Points
+// Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
 scene.add(particles);
+
 /**
  * Sizes
  */
@@ -66,7 +81,7 @@ const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
-  100
+  100,
 );
 camera.position.z = 3;
 scene.add(camera);
@@ -81,6 +96,7 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
